be29db1f3c3ff12bc5ebe81f2aa8b5584c326481
110665 <yudai.ishiguro110665@e-seikatsu.co.jp>
2020-12-10 20:17:09 +0900

    my first React Hooks with useState

--- react-hooks-101/src/App.js
+++ react-hooks-101/src/App.js
@@ -1,7 +1,22 @@
+import React, { useState } from 'react'
+
+
 const App = () => {
+  const [count, setCount] = useState(0)
+  const increase = (state) => setCount(previousCount => previousCount + 1)
+  const divide3 = (state) => setCount(previousCount => (
+    previousCount % 3 == 0 ? previousCount/3 : previousCount)
+  )
+
   return (
     <div>
-     This is a template for react App
+      <div>
+        This is a template for react App
+      </div>
+      <div>{count}</div>
+      <div>{count/3}</div>
+      <button onClick={increase}>+1</button>
+      <button onClick={divide3}>3の倍数のときだけ割る</button>
     </div>
   )
 }
