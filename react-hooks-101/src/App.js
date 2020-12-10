1924924fa3b13c513a89189eb8f1afa093aea766
110665 <yudai.ishiguro110665@e-seikatsu.co.jp>
2020-12-10 21:13:28 +0900

    7. 複数の状態を管理しよう

--- react-hooks-101/src/App.js
+++ react-hooks-101/src/App.js
@@ -1,24 +1,30 @@
 import React, { useState } from 'react'
 
 
-const App = () => {
-  const [count, setCount] = useState(0)
-  const increase = (state) => setCount(previousCount => previousCount + 1)
-  const divide3 = (state) => setCount(previousCount => (
-    previousCount % 3 == 0 ? previousCount/3 : previousCount)
-  )
+const App = props => {
+
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
+
 
   return (
     <div>
-      <div>
-        This is a template for react App
-      </div>
-      <div>{count}</div>
-      <div>{count/3}</div>
-      <button onClick={increase}>+1</button>
-      <button onClick={divide3}>3の倍数のときだけ割る</button>
+      <p>{name}</p>
+      <p>現在のボタンは{price}</p>
+      <button onClick={() =>setPrice(price + 1)}>+1</button>
+      <button onClick={reset}>reset</button>
+      <input value={name} onChange={e => setName(e.target.value)}/>
     </div>
   )
+}
+
+App.defaultProps = {
+  name: 'サンプル',
+  price: 1000
 }
 
 export default App
