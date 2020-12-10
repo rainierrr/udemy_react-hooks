8b68f7d8ed846d529538deed1b71e0d28f404a99
110665 <yudai.ishiguro110665@e-seikatsu.co.jp>
2020-12-10 21:29:47 +0900

    8. 複数の状態を1つのオブジェクトに統合しよう

--- react-hooks-101/src/App.js
+++ react-hooks-101/src/App.js
@@ -1,23 +1,28 @@
-import React, { useState } from 'react'
-
+import React, { useEffect, useState } from 'react'
 
 const App = props => {
 
-  const [name, setName] = useState(props.name)
-  const [price, setPrice] = useState(props.price)
+  const [state, setState] = useState(props)
   const reset = () => {
-    setPrice(props.price)
-    setName(props.name)
+    setState(props)
   }
 
+  useEffect(() => {
+    console.log('This is like componentDidMount or componentDidUpdate')
+  })
+
+  const renderPeriod = () => {
+    console.log('renderPeriod renders period.')
+    return '.'
+  }
 
   return (
     <div>
-      <p>{name}</p>
-      <p>現在のボタンは{price}</p>
-      <button onClick={() =>setPrice(price + 1)}>+1</button>
+      <p>{state.name}</p>
+      <p>現在の値段は{state.price}{renderPeriod()}</p>
+      <button onClick={() =>setState({...state, price: state.price + 1})}>+1</button>
       <button onClick={reset}>reset</button>
-      <input value={name} onChange={e => setName(e.target.value)}/>
+      <input value={state.name} onChange={e => setState({...state, name: e.target.value})}/>
     </div>
   )
 }
