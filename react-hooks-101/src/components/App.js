7d6f78e89948dac09b38d5cf0afa2fb63da800cd
110665 <yudai.ishiguro110665@e-seikatsu.co.jp>
2020-12-12 21:50:49 +0900

    コンポーネントわけのミスを修正

--- react-hooks-101/src/components/App.js
+++ react-hooks-101/src/components/App.js
@@ -1,9 +1,11 @@
 import React, { useReducer } from 'react'
 import { DELETE_EVENT } from '../actions'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
 import reducer from '../reducers'
 import EventForm from './EventForm'
+import Events from './Events'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, [])
@@ -11,41 +13,7 @@ const App = () => {
   return (
     <div className="container">
       <EventForm state={state} dispatch={dispatch} />
-      <h4>イベント一覧</h4>
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>タイトル</th>
-            <th>ボディー</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            state.map((event, index) => {
-              const id = event.id
-
-              const handleClickDelete = () => {
-                if (window.confirm(`イベント(id=${id})を削除しても良いですか?`)) {
-                  dispatch({
-                    type: DELETE_EVENT,
-                    id
-                  })
-                }
-              }
-              return (
-                <tr key={index}>
-                  <th>{id}</th>
-                  <th>{event.title}</th>
-                  <th>{event.body}</th>
-                  <th><button type="button" className="btn btn-danger" onClick={handleClickDelete}>削除</button></th>
-                </tr>
-              )
-            })
-          }
-        </tbody>
-      </table>
+      <Events state={state} dispatch={dispatch} />
     </div>
   )
 }
