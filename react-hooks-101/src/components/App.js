484d177e5761a50af4786ea33f05d2ffc8a755ed
110665 <yudai.ishiguro110665@e-seikatsu.co.jp>
2020-12-12 15:34:55 +0900

    13. useReducerでreducerを実際に使ってみよう

--- react-hooks-101/src/components/App.js
+++ react-hooks-101/src/components/App.js
@@ -1,45 +1,60 @@
-import React, { useEffect, useState } from 'react'
+import React, { useReducer, useState } from 'react'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-const App = props => {
+import reducer from '../reducers'
 
-  const [state, setState] = useState(props)
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, [])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  const addEvent = e => {
+    e.preventDefault()
+
+    dispatch({
+     type: 'CREATE_EVENT',
+     title,
+     body
+    })
+    setTitle('')
+    setBody('')
+  }
+
 
   return (
-    <div className='container　'>
+    <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
       <form>
-        <div className='form-group'>
-          <label htmlFor='formEventTitle'>タイトル</label>
-          <input className='form-control' id='formEventTitle'/>
+        <div className="form-group">
+          <label htmlFor="formEventTitle">タイトル</label>
+          <input className="form-control" id="formEventTitle" value={title} onChange={e => setTitle(e.target.value)}/>
         </div>
-        <div className='form-group'>
-          <label htmlFor='formEventBody'>ボディー</label>
-          <textarea className='form-control' id='formEventBody'/>
-          <button className='btn btn-primary'>イベントを作成する</button>
-          <button className='btn btn-danger'>すべてのイベントを削除する</button>
-        </div>
-      </form>
-      <h4>イベント一覧</h4>
-       <table className='table table-hober'>
-         <thead>
-           <tr>
-             <th>ID</th>
-             <th>タイトル</th>
-             <th>ボディー</th>
-           </tr>
-         </thead>
-         <tbody>
 
-         </tbody>
-       </table>
+        <div className="form-group">
+          <label htmlFor="formEventBody">ボディー</label>
+          <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)}/>
+        </div>
+
+        <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
+        <button className="btn btn-danger">全てのイベントを削除する</button>
+      </form>
+
+      <h4>イベント一覧</h4>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>タイトル</th>
+            <th>ボディー</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </table>
     </div>
   )
-}
-
-App.defaultProps = {
-  name: 'サンプル',
-  price: 1000
 }
 
 export default App
