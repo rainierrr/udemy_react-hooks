5cd9f117b8ae4627d8468ee3c424a996bf3e165c
110665 <yudai.ishiguro110665@e-seikatsu.co.jp>
2020-12-12 16:02:10 +0900

    14. イベント一覧に作成したイベントを表示させよう

--- react-hooks-101/src/components/App.js
+++ react-hooks-101/src/components/App.js
@@ -3,6 +3,7 @@ import React, { useReducer, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import reducer from '../reducers'
+import events from '../reducers'
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, [])
@@ -20,7 +21,6 @@ const App = () => {
     setTitle('')
     setBody('')
   }
-
 
   return (
     <div className="container-fluid">
@@ -51,6 +51,25 @@ const App = () => {
           </tr>
         </thead>
         <tbody>
+          {
+            state.map((event, index) => {
+              const id = event.id
+              const handleClickDelete = () => {
+                dispatch({
+                  type: "DELETE_EVENT",
+                  id
+                })
+              }
+              return (
+                <tr key={index}>
+                  <th>{id}</th>
+                  <th>{event.title}</th>
+                  <th>{event.body}</th>
+                  <th><button type="button" className="btn btn-danger" onClick={handleClickDelete}>削除</button></th>
+                </tr>
+              )
+            })
+          }
         </tbody>
       </table>
     </div>
